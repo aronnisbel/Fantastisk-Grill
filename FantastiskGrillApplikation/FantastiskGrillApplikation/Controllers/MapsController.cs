af32fa34c6e9f18a26f5dd8ed526b33f3d73ff86
HeavyKladdkaka <aron.nisbel@hotmail.com>
2018-05-15 15:26:25 +0200

    Fixade en maps-vy som startar på fantastisk grill. Försöker nu fixa pin.

---
+++ FantastiskGrillApplikation/FantastiskGrillApplikation/Controllers/MapsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FantastiskGrillApplikation.Controllers
+{
+    public class MapsController : Controller
+    {
+        // GET: Maps
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
